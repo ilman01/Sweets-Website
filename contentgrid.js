27c193344220b0eca1208f23f9d0bf88786b6c23
ilman01 <ilmanzidni2006@gmail.com>
2025-02-04 13:54:54 +0700

    jpg instead of png

--- contentgrid.js
+++ contentgrid.js
@@ -3,7 +3,7 @@ const contentgrid = `
     <div class="row g-4 d-flex align-items-center justify-content-center">
         <div class="col-lg-4 cell">
             <div onclick="location.href='/video/My-2024-Medley.html';" class="card clickable">
-                <img src="/thumbnails/medley thumbnail - 2024.png" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
+                <img src="/thumbnails/medley thumbnail - 2024.jpg" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
                 <div class="card-body">
                     <h5 class="card-title"><a class="search-title text-decoration-none text-black">My 2024 Medley</a></h5>
                     <p class="card-text">Here's a compilation of my favorite moments in 2024.</p>
@@ -13,7 +13,7 @@ const contentgrid = `
 
         <div class="col-lg-4 cell">
             <div onclick="location.href='/video/My-2023-Medley.html';" class="card clickable">
-                <img src="/thumbnails/medley thumbnail - 2023.png" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
+                <img src="/thumbnails/medley thumbnail - 2023.jpg" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
                 <div class="card-body">
                     <h5 class="card-title"><a class="search-title text-decoration-none text-black">My 2023 Medley</a></h5>
                     <p class="card-text">Here's a compilation of my favorite moments in 2023.</p>
