f11035304bb0b4f9ab9737ef2585aec50108f304
ilman01 <ilmanzidni2006@gmail.com>
2025-02-09 08:06:11 +0700

    Thumbnail to webp

--- contentgrid.js
+++ contentgrid.js
@@ -3,7 +3,7 @@ const contentgrid = `
     <div class="row g-4">
         <div class="col-lg-4 cell">
     <div onclick="location.href='/video/If-You-Have-a-Dream-Reprise/';" class="card clickable">
-        <img src="/thumbnails/If-You-Have-a-Dream-Reprise.jpg" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
+        <img src="/thumbnails/If-You-Have-a-Dream-Reprise.webp" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
         <div class="card-body">
             <h6 class="search-show text-muted fw-light">Fancy Nancy</h6>
             <h5 class="card-title"><a class="search-title text-decoration-none text-black">If You Have a Dream (Reprise)</a></h5>
@@ -13,7 +13,7 @@ const contentgrid = `
 </div>
 <div class="col-lg-4 cell">
     <div onclick="location.href='/video/My-2024-Medley/';" class="card clickable">
-        <img src="/thumbnails/My-2024-Medley.jpg" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
+        <img src="/thumbnails/My-2024-Medley.webp" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
         <div class="card-body">
             <h6 class="search-show text-muted fw-light">Original Remix</h6>
             <h5 class="card-title"><a class="search-title text-decoration-none text-black">My 2024 Medley</a></h5>
@@ -23,7 +23,7 @@ const contentgrid = `
 </div>
 <div class="col-lg-4 cell">
     <div onclick="location.href='/video/Grow-With-Me/';" class="card clickable">
-        <img src="/thumbnails/Grow-With-Me.jpg" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
+        <img src="/thumbnails/Grow-With-Me.webp" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
         <div class="card-body">
             <h6 class="search-show text-muted fw-light">Alice's Wonderland Bakery</h6>
             <h5 class="card-title"><a class="search-title text-decoration-none text-black">Grow With Me</a></h5>
@@ -33,7 +33,7 @@ const contentgrid = `
 </div>
 <div class="col-lg-4 cell">
     <div onclick="location.href='/video/My-2023-Medley/';" class="card clickable">
-        <img src="/thumbnails/My-2023-Medley.jpg" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
+        <img src="/thumbnails/My-2023-Medley.webp" class="card-img-top" style="height: 360; width: 640;" alt="Thumbnail">
         <div class="card-body">
             <h6 class="search-show text-muted fw-light">Original Remix</h6>
             <h5 class="card-title"><a class="search-title text-decoration-none text-black">My 2023 Medley</a></h5>
